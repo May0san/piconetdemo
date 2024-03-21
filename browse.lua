aa848dfd3827e9cb1806c03067503987b0bb422c
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-20 22:05:35 -0600

    Update browse.lua

--- browse.lua
+++ browse.lua
@@ -3,17 +3,63 @@
 
 --				<div style="margin-left:10px; cursor:pointer"
 p = {
-	get_gui = function(self,explorer)
-		local g = create_gui({x=0,y=0,
-			width=explorer.current_width,height=400,
-			fgcol = 0x090d})
-		g:attach_button({x=0,y=0,label="no page content",
-			bgcol=0x0706, fgcol=0x090d,
-			tap=function()
-				--load_page("pages/home.lua")
+	g,
+	title = "Browse",
+	sites = {},
+	init = function(self, explorer)
+		
+		local n = 1
+		while true do
+			local file = fetch("https://www.lexaloffle.com/bbs/?page="..n.."&tid=140647")
+			if file == nil then
+				break
 			end
-		})
-		g.
-		return g
+			local list = split(file,"\n")
+			for i=1,#list do
+				if list[i] == "				<div style=\"margin-left:10px; cursor:pointer\"" then
+					for j=i,i+8 do
+						local line = split(file[j]," ")
+						for k=1,#line do
+							if line[k] == "[PICOSITE]" then
+								local l = k+2
+								local about = ""
+								while line[l] != "[PICOSITE]" or l > #line do
+									about = about.." "..line[l]
+									l += 1
+								end
+								add(self.sites,{
+									link = line[k+1],
+									title = line[k+2],
+									about = about
+								})
+								break
+							end
+						end
+					end
+					i += 8
+				end
+			end
+			n+=1
+		end
+
+		self.g = create_gui({x=0,y=0,
+			width=300,height=(#self.sites*50)+150,
+			fgcol = 0x090d})
+		for i = #self.sites,0,-1 do
+			g:attach_button({x=5,y=(i*50)+60,label=self.sites[i].title,
+				bgcol=0x0706, fgcol=0x090d,
+				tap=function()
+					explorer:open_page(self.sites[i].link)
+				end
+			})
+		end
+	end,
+	get_gui = function(self,explorer)
+		return self.g
+	end,
+	draw = function(self,explorer)
+		for i = #self.sites,0,-1 do
+			print(self.sites[i].about, 10, (i * 50) + 75)
+		end
 	end
 }
