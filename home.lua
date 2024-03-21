6d64d20d92532feb4a5cb0a1d9e64361cd0e3ea6
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-21 07:43:03 -0600

    Update home.lua

--- home.lua
+++ home.lua
@@ -7,7 +7,7 @@ p = {
 		local g = create_gui({x=0,y=0,
 			width=300,height=200-28,
 			fgcol = 0x090d})
-		g:attach_button({x=125,y=128,width=50,label="browse",
+		self.browse = g:attach_button({x=(explorer.current_width/2)-25,y=(explorer.current_height/2) + 28,width=50,label="browse",
 			click = function()
 				explorer:open_page("https://raw.githubusercontent.com/May0san/piconetdemo/main/browse.lua")
 			end
@@ -17,14 +17,20 @@ p = {
 	draw = function(self, explorer)
 		cls(7)
 		palt(7)
-		circfill(150,72,40,1)
-		ovalfill(112,34,188,100,16)
-		ovalfill(126,36,174,62,12)
-		ovalfill(140,40,160,47,28)
+		local center_x = explorer.current_width/2--default 150
+		local center_y = (explorer.current_height/2) + 18--default 118
+		circfill(center_x,center_y-46,40,1)
+		ovalfill(center_x-38,center_y-84,center_x+38,center_y-18,16)
+		ovalfill(center_x-24,center_y-82,center_x+24,center_y-56,12)
+		ovalfill(center_x-10,center_y-78,center_x+10,center_y-71,28)
 		spr(--[[pod,pod_type="image"]]unpod("b64:bHo0AI8AAABoAQAA8x1weHUAQyBAEAT-EDFwDQ5gDQ5QDR5gDQ4gDR4gDWANYA0OgA1gDXANgA1ADREAARsAAwMAIRANCACCgA0gDV4gDS4VAAEDABFOBgA-IA1AGwAHEB4eAB8_HAAiAlQAP4ANHhkAAz9wDS4ZAAM-IA1_GQAkBOsACRsAEl4bAQMtARQeFAAKRAFQHiAN-jI="),
-		119,68)
-		print("Explore\n	The",101,47,13)
-		print("Explore\n	The",100,47,0)
+		center_x-31,center_y-50)
+		print("Explore\n	The",center_x-49,center_y-71,13)
+		print("Explore\n	The",center_x-50,center_y-71,0)
 		palt()
+	end,
+	update = function(self, explorer)
+		self.browse.x = (explorer.current_width/2)-25
+		self.browse.y=(explorer.current_height/2) + 28
 	end
 }
