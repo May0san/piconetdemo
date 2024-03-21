9a78e54d095b1f1ef8a9878d7b8754a4319377d8
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-21 04:37:42 -0600

    Update home.lua

--- home.lua
+++ home.lua
@@ -7,12 +7,25 @@ p = {
 		local g = create_gui({x=0,y=0,
 			width=300,height=200-28,
 			fgcol = 0x090d})
-		g:attach_button({x=100,y=0,label="home",
-			bgcol=0x0706, fgcol=0x090d
+		g:attach_button({x=125,y=128,width=50,label="browse",
+			bgcol=0x0706, fgcol=0x090d,
+			click = function()
+				explorer:open_page("https://raw.githubusercontent.com/May0san/piconetdemo/main/browse.lua")
+			end
 		})
 		return g
 	end,
 	draw = function(self, explorer)
-		cls(12)
+		cls(7)
+		palt(7)
+		circfill(150,72,40,1)
+		ovalfill(112,34,188,100,16)
+		ovalfill(126,36,174,62,12)
+		ovalfill(140,40,160,47,28)
+		spr(--[[pod,pod_type="image"]]unpod("b64:bHo0AI8AAABoAQAA8x1weHUAQyBAEAT-EDFwDQ5gDQ5QDR5gDQ4gDR4gDWANYA0OgA1gDXANgA1ADREAARsAAwMAIRANCACCgA0gDV4gDS4VAAEDABFOBgA-IA1AGwAHEB4eAB8_HAAiAlQAP4ANHhkAAz9wDS4ZAAM-IA1_GQAkBOsACRsAEl4bAQMtARQeFAAKRAFQHiAN-jI="),
+		119,68)
+		print("Explore\n	The",101,47,13)
+		print("Explore\n	The",100,47,0)
+		palt()
 	end
 }
