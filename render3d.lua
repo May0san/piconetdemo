19a2df1d9109d4d9e36995ab669249cab841f9d9
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-21 01:39:54 -0600

    Update render3d.lua

--- render3d.lua
+++ render3d.lua
@@ -7,6 +7,7 @@ p = {
 		self.g:attach_button({x=0,y=230,label="Download 3D Render",
 			bgcol=0x0706, fgcol=0x090d,
 			tap=function()
+				cd("/drive/downloads")
 				local file = fetch("https://github.com/May0san/piconetdemo/raw/main/amateur_3d.p64.png")
 				store("amateur_3d.p64.png",file,{})
 			end
