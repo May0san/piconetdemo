dd6a159d5009df392cd104d758bb1830896768ed
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-20 21:32:17 -0600

    Add files via upload

---
+++ browse.lua
@@ -0,0 +1,19 @@
+--[[pod_format="raw",created="2024-03-21 03:18:50",modified="2024-03-21 03:19:41",revision=4]]
+
+
+--				<div style="margin-left:10px; cursor:pointer"
+p = {
+	get_gui = function(self,explorer)
+		local g = create_gui({x=0,y=0,
+			width=explorer.current_width,height=400,
+			fgcol = 0x090d})
+		g:attach_button({x=0,y=0,label="no page content",
+			bgcol=0x0706, fgcol=0x090d,
+			tap=function()
+				--load_page("pages/home.lua")
+			end
+		})
+		g.
+		return g
+	end
+}
