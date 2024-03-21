85a311b371cd508ab9867fa0d36421d2669bda0d
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-21 02:41:15 -0600

    Update browse.lua

--- browse.lua
+++ browse.lua
@@ -14,7 +14,7 @@ p = {
 		local n = 1
 		while true do
 			local times_added = 0
-			local file = fetch("https://www.lexaloffle.com/bbs/?page="..n.."&tid=140647")
+			local file = fetch("https://www.lexaloffle.com/bbs/?tid=144018")
 			
 			local list = split(file,"\n")
 			for i=1,#list do
