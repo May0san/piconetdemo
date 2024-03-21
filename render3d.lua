87db959b6faabde662e30bba3c9fc5db0b81acda
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-21 16:22:22 -0600

    Update render3d.lua

--- render3d.lua
+++ render3d.lua
@@ -8,7 +8,7 @@ p = {
 			tap=function()
 				--these two lines dowload a file, but I'm pretty sure you need to have a downloads folder in your root already.
 				--I also want to create a function for this in the near future to simplify it down to something like "download(link, [path or downloads folder])"
-				download("https://github.com/May0san/piconetdemo/raw/main/amateur_3d.p64.png","/downloads","amateur_3d.p64.png")
+				download("https://github.com/May0san/piconetdemo/raw/main/amateur_3d.p64.png","amateur_3d.p64.png","/downloads")
 			end
 		})
 		self.g:attach_button({x=2,y=2,label="Go Back",
