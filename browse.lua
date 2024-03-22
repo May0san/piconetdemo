a7e1d2ba9e19edbb884130a1713481caace57b34
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-22 17:27:45 -0600

    Update browse.lua

--- browse.lua
+++ browse.lua
@@ -11,7 +11,7 @@ p = {
 	sites = {},
 	--debug = {},
 	init = function(self, explorer)
-		self.g:attach_button({x=250,y=5,label="build a site!",
+		self.g:attach_button({x=explorer.current_width-100,y=5,label="build a site!",
 			tap=function()
 				explorer:open_page("https://raw.githubusercontent.com/May0san/piconetdemo/main/sitebuilder.lua")
 			end
