05e3100f9e7da1d1621edefe5cb168ac668fae36
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-21 18:30:50 -0600

    Update coolgifs.lua

--- coolgifs.lua
+++ coolgifs.lua
@@ -22,8 +22,8 @@ p = {
 			"your own pages! There's also some directions\n"..
 			"there on how to make your own (it's easy!)\n\n"..
 			"Here you go:", 10,0,7)
-		self.spin_cartridge:draw(2,2)-- you only need to specify position when drawing gif
-		self.smile:draw(18,2)
+		self.spin_cartridge:draw(2,280)-- you only need to specify position when drawing gif
+		self.smile:draw(20,2)
 	end,
 	--gifs use a pod. you can copy it directly from the gfx editor. The format is one frame tall by however many frames you have long
 	spin_cartridge = new_gif(unpod("b64:bHo0AGIBAADTAwAA8zBweHUAQyhgAQAAEAAAAATw--_1AZ8QYA8gAW8QcBFOkB4hsAEfILARwB0BsCEekE4RcG4BDVCeAUABnlANAW4jAHkeDdAe0A0eIgDwEjABrlANAX5gEV6AHjGgHh2wHsAdHqAxHoBeEWB_AQ1AriIAGEAiAB-AIgAA-wwOeA8YDlANAQ5IDA5gEQ4oDA6AHgEcAaAeAg1RABLxCw0CHqABHAEegA4MKA4RYA4MSA4BDUAOeAwOXQAfDFwA-g_1AS4P_QE1IHEe4wAgQQ3jACAhDeMALx0BTgAYIAEd4QAgDSHhACENQeEAIHEeWAAgYS5YAO8xDR5gEQ4RDR6AHgENEVgAGSANEVgAIBEeWAAgMR5YAHRhLgEwAZ5gDwMXMU4ADzEDAQhOADcxHoAiAJGOcA0BXoARPqBTAxPAUwPioD4RgF4BDWCOAVABjmAiAAGYAyQdwJcDBCIAUAHw--_2"),
