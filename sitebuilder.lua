caff9f73c8224304adc0695cafdf50b8e7d6eeb9
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-25 00:41:25 -0600

    Update sitebuilder.lua

--- sitebuilder.lua
+++ sitebuilder.lua
@@ -45,10 +45,10 @@ p = {
 		}
 	},
 	selection_gui = create_gui({x=0,y=28 + 15,-- update but don't draw
-		width=300,height=200-28,
+		width=300,height=200-13,
 		fgcol = 0x090d}),
 	page_mockup = create_gui({x=0,y=0,-- draw but don't update
-		width=300,height=200-28,
+		width=300,height=200-13,
 		fgcol = 0x090d}),
 	selected_gui,
 	init = function(self,explorer)
