db35be1aea177130f3c714eb38909be346d938fc
Meivuu <70550220+May0san@users.noreply.github.com>
2024-03-25 00:13:03 -0600

    Update games.lua

--- games.lua
+++ games.lua
@@ -27,11 +27,11 @@ p = {
 	draw = function(self,explorer) -- use this to draw, write text, etc. You can use cls() without issues as well if you want a different bg
 		print("\n\nWELCOME to my\nCOOL GAMES WEBSITE", 0,0,7)
 		print("Play my AWESOME games:", 0, 80,7)
-		self.mygif:draw(2,2,3,0)-- you need to specify position, speed, and alpha when drawing gif
-		self.mygif:draw(18,2,3,0)
-		self.mygif:draw(34,2,3,0)
-		self.mygif:draw(50,2,3,0)
-		self.smile:draw(120,74,1,0)
+		self.mygif:draw(2,2,16,16,3,0)-- you need to specify position, speed, draw width/height and alpha when drawing gif
+		self.mygif:draw(18,2,16,16,3,0)
+		self.mygif:draw(34,2,16,16,3,0)
+		self.mygif:draw(50,2,16,16,3,0)
+		self.smile:draw(120,74,16,16,1,0)
 	end,
 	--gifs use a pod. you can copy it directly from the gfx editor, and keep only the string. The format is one frame tall by however many frames you have long
 	mygif = new_gif("b64:bHo0AGIBAADTAwAA8zBweHUAQyhgAQAAEAAAAATw--_1AZ8QYA8gAW8QcBFOkB4hsAEfILARwB0BsCEekE4RcG4BDVCeAUABnlANAW4jAHkeDdAe0A0eIgDwEjABrlANAX5gEV6AHjGgHh2wHsAdHqAxHoBeEWB_AQ1AriIAGEAiAB-AIgAA-wwOeA8YDlANAQ5IDA5gEQ4oDA6AHgEcAaAeAg1RABLxCw0CHqABHAEegA4MKA4RYA4MSA4BDUAOeAwOXQAfDFwA-g_1AS4P_QE1IHEe4wAgQQ3jACAhDeMALx0BTgAYIAEd4QAgDSHhACENQeEAIHEeWAAgYS5YAO8xDR5gEQ4RDR6AHgENEVgAGSANEVgAIBEeWAAgMR5YAHRhLgEwAZ5gDwMXMU4ADzEDAQhOADcxHoAiAJGOcA0BXoARPqBTAxPAUwPioD4RgF4BDWCOAVABjmAiAAGYAyQdwJcDBCIAUAHw--_2",
